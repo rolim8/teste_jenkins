ff3d0b734da6f43d9f3804bbfea5b318bf075fb5
Orlando Rolim <rolimorlando@gmail.com>
2020-11-24 21:04:33 -0300

    Primeiro Commit

---
+++ src/main/java/pessoa.java
@@ -0,0 +1,25 @@
+public class pessoa {
+    private String nome;
+    private int cpf;
+
+    public pessoa(String nome, int cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+}
